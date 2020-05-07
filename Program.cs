24a675a5d366e0e74173191e6af94771915072c1
Ben Taylor <ben@bentaylor.work>
2020-05-07 12:32:28 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+
+namespace restpdf.io
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ApiKey = "";
+
+            var RequestBodyParameters = new
+            {
+                output = "data",
+                url    = "https://www.google.co.uk"
+            };
+
+            var RequestBodyBody = new StringContent(JsonSerializer.Serialize(RequestBodyParameters), Encoding.UTF8, "application/json");
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("X-API-KEY", ApiKey);
+
+                using (var result = client.PostAsync("https://api.restpdf.io/v1/pdf", RequestBodyBody).Result)
+                {
+                    if (result.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("Your PDF was converted.");
+
+                        File.WriteAllBytes("google.pdf", result.Content.ReadAsByteArrayAsync().Result);
+                    } else
+                    {
+                        Console.WriteLine("There was an error converting your PDF.");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# RestPdf.io  - Example-Dot-Net-Core-3.1
+
+## Overview
+The example .NET Core (3.1) console application in this GitHub repository shows you how to generate PDF's using the RestPdf.io API that converts HTML to PDF's.
+
+## Console Application
+
+To use this example the following variables will need to be filled out.
+
+```
+var ApiKey = "<api-key>";
+```
+
+## FAQ
+* <b>Where can I get an API key?</b> You can generate an API key by signing up to RestPdf.io API at https://app.restpdf.io where you will get 50 PDF free PDF conversions a month. If you need more than 50 conversions a month, we offer monthly paid plans.
+
+## Contact Information
+- www.restpdf.io
+- info@restpdf.io
